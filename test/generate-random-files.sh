71ae6a62b14a84d45821aa80595f4360cb14ddd1
Andrew Kester <akester@cox.net>
2013-04-09 09:29:04 -0500

    test-data: adding header to random file script

--- test/generate-random-files.sh
+++ test/generate-random-files.sh
@@ -1,8 +1,12 @@
+# A script to generate files with random file sizes.
+# Idea taken from rajaseelan.com, adapted by Andrew Kester
+# Copyright 2013 Andrew Kester, Released under GNU-GPLv3
+
 files=$1;
 outdir=$2
 
 x=1;
-while [[ $x -le $files ]]; 
+while [[ $x -le $files ]];
  do echo Creating file no $x;
   size=`shuf -i 1-100 -n 1`
   dd bs=1024 count=$size skip=$RANDOM if=/dev/urandom of=$outdir/random-file.$x 1> /dev/null;
