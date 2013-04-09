e9cbeff7ccfafc5df31033170194ea92ed05b696
Andrew Kester <akester@cox.net>
2013-04-09 09:52:21 -0500

    program: adding main python script with headers.

---
+++ src/backup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+A Program to create chunked snapshot backups and encrpyt the resulting files.  Will also
+Manage the files in a local or seperate file system, allowing for the encrpyted files
+to be stored in an offiste filesystem (either a custom setup or a service like Amazon
+Glacier)
+
+This program is copyright 2013 Andrew Kester, released under the GNU General Public License
+version 3 or, at your option, any later version.
+"""
+
