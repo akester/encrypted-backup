68d0e54b52daf26b03a59ff7d3637149da4bc36f
Andrew Kester <akester@cox.net>
2013-04-09 09:22:07 -0500

    test-data: limiting file size to 100K

--- test/generate-random-files.sh
+++ test/generate-random-files.sh
@@ -4,7 +4,7 @@ outdir=$2
 x=1;
 while [[ $x -le $files ]]; 
  do echo Creating file no $x;
-  size=`shuf -i 1-500 -n 1`
+  size=`shuf -i 1-100 -n 1`
   dd bs=1024 count=$size skip=$RANDOM if=/dev/urandom of=$outdir/random-file.$x 1> /dev/null;
   let "x += 1";
  done
