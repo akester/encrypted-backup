a24b30841a6308ff686d38cf390e660fc267a5cf
Andrew Kester <akester@cox.net>
2013-04-09 10:11:14 -0500

    program: adding hollow classes and blank argument parsing.

--- src/backup.py
+++ src/backup.py
@@ -10,3 +10,34 @@ This program is copyright 2013 Andrew Kester, released under the GNU General Pub
 version 3 or, at your option, any later version.
 """
 
+## Imports
+from multiprocessing import Pool
+import argparse
+import sqlite3
+import ConfigParser
+
+"""
+Main EB Functions
+"""
+class EBMain:
+    pass
+
+"""
+Threading functions and operations
+"""
+class EBThreading:
+    pass
+
+"""
+Database operations
+"""
+class EBDatabase:
+    pass
+
+
+if __name__ == '__main__':
+    ## Argument Parsing
+    parser = argparse.ArgumentParser(description="A program to create snapshot backups.")
+    
+    args = parser.parse_args()
+    
