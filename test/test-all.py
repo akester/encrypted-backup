88ca7cfc98f5b27a54688b1c69bc9917740165d6
Andrew Kester <akester@cox.net>
2013-04-09 17:05:39 -0500

    test-data: adding more helpful error text to some tests

--- test/test-all.py
+++ test/test-all.py
@@ -41,11 +41,14 @@ class testEBFull(unittest.TestCase):
     # Test file hashing
     def test_fileHashing(self):
         # Hash a file
-        self.assertEqual(self.ebm.getFileHash('files/hacking.txt'), '142ed81e48888c141c64e27215d99ce14d52f829')
+        self.assertEqual(self.ebm.getFileHash('files/hacking.txt'), '142ed81e48888c141c64e27215d99ce14d52f829',
+                         'Test data is not consistent')
         # Hash a small directory
-        self.assertEqual(self.ebm.getDirHash('directory/10'), '1337b77ae713094ad3d6b99302cd08a46d9fce5f')
+        self.assertEqual(self.ebm.getDirHash('directory/10'), '1337b77ae713094ad3d6b99302cd08a46d9fce5f',
+                         'Test data is not consistent')
         # Hash a larger directory
-        self.assertEqual(self.ebm.getDirHash('directory'), '0dfcc0df20c63b1a010e8a4d14f3208a6f6df32f')
+        self.assertEqual(self.ebm.getDirHash('directory'), '0dfcc0df20c63b1a010e8a4d14f3208a6f6df32f',
+                         'Test data is not consistent')
         
         
     # Check tarball compression
@@ -57,7 +60,9 @@ class testEBFull(unittest.TestCase):
         except:
             self.fail('Could not extract tar archive')
             
-        # 
+        # Ensure that we have the same data
+        self.assertEqual(self.ebm.getDirHash('tmp/directory/10'), '1337b77ae713094ad3d6b99302cd08a46d9fce5f',
+                         'Extracted data is not consistent')
             
         #Clean the files
         os.remove('tmp/10.tar')
