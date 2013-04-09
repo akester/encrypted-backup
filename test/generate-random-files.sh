df07ab2c46c091a90f8b823fd7a8411196757ab5
Andrew Kester <akester@cox.net>
2013-04-09 09:16:28 -0500

    test-data: fixing random file script
    A misnamed variable wrote all of the output files into the same file.

--- test/generate-random-files.sh
+++ test/generate-random-files.sh
@@ -4,6 +4,6 @@ outdir=$2
 x=1;
 while [[ $x -le $files ]]; 
  do echo Creating file no $x;
-  dd bs=1024 count=$RANDOM skip=$RANDOM if=/dev/urandom of=$outdir/random-file.$counter 1> /dev/null;
+  dd bs=1024 count=$RANDOM skip=$RANDOM if=/dev/urandom of=$outdir/random-file.$x 1> /dev/null;
   let "x += 1";
  done
