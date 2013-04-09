5bf48f02c9b8565a92e373d4dec215105c2f15b5
Andrew Kester <akester@cox.net>
2013-04-09 13:41:00 -0500

    test-data: adding hollow unit testing

---
+++ test/test-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+Unit testing for EB.  Please keep this file tidy.
+Copyright 2013 Andrew Kester, released under GNU GPLv3
+"""
+
+import unittest
+
+class testEBFull(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
