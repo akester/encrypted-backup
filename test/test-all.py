9c406d8f2788e5d8220407aa6d68bd4cba3138f9
Andrew Kester <akester@cox.net>
2013-04-09 13:45:12 -0500

    test-data: adding EB specific classes to unit testing

--- test/test-all.py
+++ test/test-all.py
@@ -7,8 +7,17 @@ Copyright 2013 Andrew Kester, released under GNU GPLv3
 
 import unittest
 
+# Append our path so we can include the EB classes
+import sys
+sys.path.append('../src')
+
+import backup
+
 class testEBFull(unittest.TestCase):
-    pass
+    # Create an instance of all of the EB classes
+    ebm = backup.EBMain()
+    ebt = backup.EBThreading()
+    ebd = backup.EBDatabase()
 
 if __name__ == '__main__':
     unittest.main()
