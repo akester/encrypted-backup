7768f7afa38e69912ad653282ab4cb00832aaa06
Andrew Kester <akester@cox.net>
2013-04-23 22:45:33 -0500

    eb: deleting all unused code.

--- src/backup.py
+++ src/backup.py
@@ -11,13 +11,8 @@ Public License version 3 or, at your option, any later version.
 import argparse
 import ConfigParser
 import gnupg
-#import gzip
-#import hashlib
-#import multiprocessing 
 import os
 import shutil
-#import signal
-#import sqlite3
 import subprocess
 import sys
 import tarfile
@@ -33,12 +28,6 @@ class EBMain:
     """
     # The location of the configuration files (default: /etc/eb/eb.conf
     configFileLocation = '../cfg/eb.conf'
-    
-#    """
-#    Converts an epoch time to a string
-#    """
-#    def getDate(self, epoch):
-#        return time.ctime(epoch)
     
     """
     A wrapper for configuration file parsing
@@ -63,12 +52,6 @@ class EBMain:
                 
         return out
     
-#    """
-#    Generates the hash for this snapshot
-#    """
-#    def generateSnapshotHash(self, path):
-#        return hashlib.sha1(path).hexdigest()
-    
     """
     Archive a directory
     """
@@ -76,56 +59,7 @@ class EBMain:
         tar = tarfile.open(outfile, "w")
         tar.add(directory)
         tar.close()
-        
-#    """
-#    Extracts a tar archive
-#    """        
-#    def dearchiveDirectory(self, tar, outpath):
-#        tar = tarfile.open(tar)
-#        tar.extractall(outpath)
-#        tar.close()
-        
-#    """
-#    Gets the hash of a file
-#    """
-#    def getFileHash(self, path):
-#        sha1 = hashlib.sha1()
-#        f = open(path)
-#        try:
-#            sha1.update(f.read())
-#        finally:
-#            f.close()
-#        return sha1.hexdigest()
-    
-#    """
-#    Gets the hash of a directory
-#    """
-#    def getDirHash(self, path):
-#        sha1 = hashlib.sha1()
-#        tree = os.walk(path)
-#        for root, dirs, files in tree:
-#            for f in files:
-#                sha1.update(self.getFileHash(root + '/' + f))
-#               
-#        return sha1.hexdigest()
-    
-#    """
-#    Compress a file
-#    """
-#    def compressFile(self, path, outpath):
-#        f = gzip.open(outpath, 'wb')
-#        f_in = open(path, 'rb')
-#        f.writelines(f_in)
-#        f.close()
-    
-#    """
-#    Decompress a file
-#    """
-#    def decompressFile(self, path, outpath):
-#        f = gzip.open(path)
-#        f_out = open(outpath, 'w+')
-#        f_out.writelines(f.read())
-#        
+
     """
     Get the size of a file
     """
@@ -158,29 +92,6 @@ class EBMain:
     def getFileName(self, path):
         return os.path.basename(path)
     
-#    def chunkFile(self, inpath, outpath, csize):
-#        size = self.getFileSize(inpath)
-#        chunks = self.calculateChunks(size, csize)
-#        data = self.getFileData(inpath)
-#        filename = self.getFileName(inpath)
-#        
-#        # Create an output directory
-#        if not os.path.isdir(outpath):
-#            os.mkdir(outpath)
-#        
-#        # Write the output files
-#        i = 1
-#        while i <= chunks:
-#            outName = outpath + '/' + filename + '.' + str(i)
-#            
-#            f = open(outName, 'wb')
-#            f.write(data[i:i + csize])
-#            f.close()
-#            
-#            i += 1
-#            
-#        return chunks
-    
     """
     Another method of chunking files, this one uses the Linux Split commmand
     It's less cross platform friendly, but splitting large files with the above
@@ -188,7 +99,6 @@ class EBMain:
     RAM, then chunking the file.
     """
     def chunkFileSplit(self, inpath, outpath, prefix, csize):
-        #TODO: Determine number of digits and pass that to split
         size = self.getFileSize(inpath)
         chunks = self.calculateChunks(size, csize)
         
@@ -218,137 +128,7 @@ class EBMain:
     
     This function does not return data in order (a limitation of os.walk)
     """
-#    def assembleChunks(self, inpath, outpath):
-#        chunks = 0
-#        out = open(outpath, 'a+')
-#        for root, dirs, files in os.walk(inpath):
-#            for f in files:
-#                inf = open(root + '/' + f, 'r')
-#                out.write(inf.read())
-#                chunks += 1
-#                
-#        return chunks
-                
-#"""
-#Threading functions and operations
-#"""
-#class EBThreading:
-#    """
-#    Nice Keyboard interrupt handling Copyright 2011 John Reese and released
-#    under the MIT License.
-#    
-#    It allows for the user to stop a threaded job and allow the pool to
-#    terminate rather than hanging on pool.join().  This resolves a bug in 2.X 
-#    versions of python's multiprocessing module.
-#    """
-#    def __init__(self, threads = multiprocessing.cpu_count()):
-#        self.pool = multiprocessing.Pool(int(threads), self.int_worker)
-#        self.inQueue = []
-#        self.assigned = 0
-#        
-#    def int_worker(self):
-#        signal.signal(signal.SIGINT, signal.SIG_IGN)
-#        
-#    def printProgress(self):
-#        completed = self.assigned - len(self.inQueue)
-#        percent = float(completed) / self.assigned
-#        
-#        sys.stdout.write('\r{0}%...'.format(percent))
-#        
-#    def runPool(self, progress = True):
-#        try:
-#            while len(self.inQueue) > 0:
-#                if progress:
-#                    self.printProgress()
-#                time.sleep(0.10)
-#                
-#        except:
-#            # User sent SIGINT or the pool crashed. (Either way we should leave)
-#            self.pool.terminate()
-#            self.pool.join()
-#            sys.stdout.write('\n')
-#            sys.stdout.flush()
-
-#"""
-#Database operations
-#"""
-#class EBDatabase:
-#    # Location of the database file we are using.
-#    path = None
-#    db = None
-#    c = None
-#    
-#    """
-#    Connect to a database
-#    """
-#    def __init__(self, path):
-#        self.path = path
-#        self.db = sqlite3.connect(self.path)
-#        self.c = self.db.cursor()
-#    
-#    """
-#    Close the db connection
-#    """
-#    def __del__(self):
-#        self.c.close()    
-#        
-#    """
-#    Create a database to be stored with the backups
-#    """
-#    def initBackupDB(self):        
-#        self.c.execute("CREATE TABLE IF NOT EXISTS chunks (id int, name text, " 
-#                       + "run integer)")
-#        self.c.execute("CREATE TABLE IF NOT EXISTS meta (key text, value text)")
-#        self.c.execute("CREATE TABLE IF NOT EXISTS runs (date integer," 
-#                       + " status integer)")
-#        self.db.commit()
-#        
-#    """
-#    Stores a chunk
-#    """
-#    def storeChunkInformation(self, cid, name, run):        
-#        self.c.execute("INSERT INTO chunks VALUES ({0}, '{1}', {2})"
-#                       .format(cid, name, run))
-#        self.db.commit()
-#        
-#    """
-#    Gets all of the chunks and thier files
-#    """
-#    def getChunks(self):
-#        out = {}
-#        for row in self.c.execute("SELECT * FROM chunks WHERE 1"):
-#            out[row[0]] = row[1]
-#        return out
-#    
-#    """
-#    Stores metadata
-#    """
-#    def storeMeta(self, key, value):
-#        self.c.execute("INSERT INTO meta VALUES('{0}', '{1}')".
-#                       format(key, value))
-#        self.db.commit()
-#        
-#    """
-#    Gets metadata
-#    """
-#    def getMeta(self, key = '%'):
-#        out = {}
-#        for row in self.c.execute("SELECT * FROM meta WHERE key LIKE '{0}'"
-#                                  .format(key)):
-#            out[row[0]] = row[1]
-#        return out
-#    
-#    """
-#    Gets chunks by status
-#    """
-#    def getRunStatus(self, status, operator = '='):
-#        out = {}
-#        for row in self.c.execute("SELECT * FROM runs WHERE status {0} {1}"
-#                                  .format(operator, status)):
-#            out[row[0]] = row[1]
-#            
-#        return out
-#    
+ 
 """
 Encryption Functions
 """
@@ -391,9 +171,7 @@ if __name__ == '__main__':
                         help="Restore the files from --path rather than encrypt"
                         + " them.")
     args = parser.parse_args()
-    
-#    ebd = EBDatabase('../cfg/eb.sql')
-#    ebd.initBackupDB()
+
     ebm = EBMain()
     ebe = EBEncryption(os.path.expanduser('~/.gnupg'))
     
@@ -405,10 +183,6 @@ if __name__ == '__main__':
     except KeyError as e:
         sys.stderr.write('E: Missing configuration value {0}\n'.format(e))
         exit(errno.EINVAL)
-        
-                        
-    # Create threading
-#    ebt = EBThreading()
     
     if args.rest:
         # Make sure the path exists
@@ -474,8 +248,7 @@ if __name__ == '__main__':
                                  cfg['main']['keyid'], cfg['main']['passp'])
                 sys.stdout.write('Encrypted file {0} of {1}\n'
                                  .format(x, numFiles))
-                
-#        ebt.runPool(True)
+
         shutil.rmtree(args.tmppath)
             
         exit(0)
--- test/test-all.py
+++ test/test-all.py
@@ -19,24 +19,14 @@ class testEBFull(unittest.TestCase):
     # Test the imports and declarations
     def test_imports(self):
         ebm = backup.EBMain()
-#        ebt = backup.EBThreading()
-#        ebd = backup.EBDatabase('tmp/test-database.sql')
         ebe = backup.EBEncryption('tmp')
 
         try:
             self.assertIsInstance(ebm, backup.EBMain)
-#            self.assertIsInstance(ebt, backup.EBThreading)
-#            self.assertIsInstance(ebd, backup.EBDatabase)
             self.assertIsInstance(ebe, backup.EBEncryption)
         except AttributeError:
             pass
-        
-#        try:
-#            #Clean the files
-#            os.remove('tmp/test-database.sql')
-#        except:
-#            self.fail('Could not remove tmp database.')
-    
+
     # Test configuration parsing
     def test_configParsing(self):
         ebm = backup.EBMain()
@@ -66,94 +56,13 @@ class testEBFull(unittest.TestCase):
     def test_tar(self):
         ebm = backup.EBMain()
         ebm.archiveDirectory('directory/10', 'tmp/10.tar')
-        # Check that the file exists and we can decompress it (ie, it is a tar file)
-#        try:
-#            ebm.dearchiveDirectory('tmp/10.tar', 'tmp/.')
-#        except:
-#            self.fail('Could not extract tar archive')
             
         try:
             #Clean the files
             os.remove('tmp/10.tar')
- #           shutil.rmtree('tmp/directory')
         except:
             self.fail('Could not remove tmp extraction data.')
-            
-#    # Check gzip compression
-#    def test_gzip(self):
-#        ebm = backup.EBMain()
-#        ebm.compressFile('files/hacking.txt', 'tmp/hacking.txt.gz')
-#        try:
-#            ebm.decompressFile('tmp/hacking.txt.gz', 'tmp/hacking.txt')
-#        except:
-#            self.fail('Could not decompress archive.')
-#        
-#        try:
-#            #Clean the files
-#            os.remove('tmp/hacking.txt.gz')
-#            os.remove('tmp/hacking.txt')
-#        except:
-#            self.fail('Could not remove tmp extraction data.')
-            
-#    # Check database functions
-#    def test_databaseChunks(self):
-#        ebd = backup.EBDatabase('tmp/test-db-init.sql')
-#        ebm = backup.EBMain()
-#        ebd.initBackupDB()
-#        
-#        # Test some data
-#        ebd.storeChunkInformation(1, '1000-1.tar.gz', 1)
-#        
-#        # Test a read
-#        # This var should be the chunks we will get from above
-#        chunks = {1:'1000-1.tar.gz'}
-#        self.assertEqual(ebd.getChunks(), chunks,
-#                         'Data fetch was not consitent')
-#        
-#        del ebd
-#        
-#        try:
-#            #Clean the files
-#            os.remove('tmp/test-db-init.sql')
-#        except:
-#            self.fail('Could not remove tmp database data.')
-#            
-#    def test_databaseMeta(self):
-#        ebd = backup.EBDatabase('tmp/test-db-meta.sql')
-#        ebm = backup.EBMain()
-#        ebd.initBackupDB()
-#        
-#        ebd.storeMeta('testkey', 'testvalue')
-#        
-#        meta = {'testkey':'testvalue'}
-#        self.assertEqual(ebd.getMeta(), meta)
-#        
-#        del ebd
-#        
-#        try:
-#            #Clean the files
-#            os.remove('tmp/test-db-meta.sql')
-#        except:
-#            self.fail('Could not remove tmp database data.')
-#            
-#    def test_databaseRuns(self):
-#        ebd = backup.EBDatabase('tmp/test-db-runs.sql')
-#        ebm = backup.EBMain()
-#        ebd.initBackupDB()
-#        
-#        ebd.c.execute('INSERT INTO runs VALUES (0, 1)')
-#        ebd.db.commit()
-#        
-#        # This should equal the data inserted
-#        row = {0:1}
-#        self.assertEqual(row, ebd.getRunStatus(1))
-#        
-#        try:
-#            #Clean the files
-#            os.remove('tmp/test-db-runs.sql')
-#        except:
-#            self.fail('Could not remove tmp database data.')   
-#            
+           
     # Test file chunking
     def test_chunkingHelpers(self):
         ebm = backup.EBMain()
@@ -165,21 +74,6 @@ class testEBFull(unittest.TestCase):
         self.assertEqual(ebm.getFileData('files/oneline.txt'),
                          'The quick fox jumed over the lazy brown dog.')
         self.assertEqual(ebm.getFileName('files/archive.tar'), 'archive.tar')
-        
-#    def test_chunkingMain(self):
-#        ebm = backup.EBMain()
-#        self.assertEqual(ebm.chunkFile('files/archive.tar', 'tmp/archive1', 1000),
-#                         564)
-#        
-#        if not os.path.isdir('tmp/archive1'):
-#            self.fail('Output Directory Failed')
-#        
-#        
-#        # Clean the files
-#        try:
-#            shutil.rmtree('tmp/archive1')
-#        except:
-#            self.fail('Could not remove chunked file')
             
     # Test the Split chunking
     def test_chunkingSplit(self):
@@ -215,27 +109,6 @@ class testEBFull(unittest.TestCase):
             os.remove('tmp/oneline.txt')
         except:
             self.fail('Could not remove file')
-            
-            
-#    def test_fileJoins(self):
-#        ebm = backup.EBMain()
-#        self.assertEqual(ebm.chunkFile('files/oneline.txt', 'tmp/test-joins',
-#                                       10), 5)
-#        
-#        ebm.assembleChunks('tmp/test-joins', 'tmp/oneline.txt')
-#        self.assertEqual(ebm.getFileData('tmp/oneline.txt'), 
-#                         'The quick fox jumed over the lazy brown dog.')
-#        
-#        # Clean the files
-#        try:
-#            shutil.rmtree('tmp/test-joins')
-#            os.remove('tmp/oneline.txt')
-#        except:
-#            self.fail('Could not remove file')
-#            
-#    def test_date(self):
-#        ebm = backup.EBMain()
-#        self.assertEqual(ebm.getDate(123456), 'Fri Jan  2 04:17:36 1970')
     
     # Encrypt/Decrypt
     def test_encryption(self):
