ddf50d6440f7a38a25020026b213941eccc5bd44
Andrew Kester <akester@cox.net>
2013-04-24 22:04:44 -0500

    eb: adding file to run for cron.

---
+++ src/backup-cron.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#########################################
+# EB Example cron script.		#
+# Copyright 2013 Andrew Kester		#
+# Released under GNU-GPL		#
+#########################################
+
+# Set this var to set the input paths you want to backup
+backups=('/home/andrew/Projects/servus' '/home/andrew/Documents')
+
+# Set this var to the output location to store backups (subdirectories will be
+# created automatically)
+outpath='../test/tmp/'
+
+# Set this to the tmp directory you would like to use
+tmppath='/tmp/eb'
+
+# For each backup iterate through the backup and run eb on it
+for b in "${backups[@]}"
+do
+	date=`date | sed -e 's/ /-/g'`
+	ob=$outpath'/'$b'/'$date
+	mkdir -p "$ob"
+	echo "Backing up $b into $ob"
+	if [ $? -ne 0 ]
+	then
+		echo "E: Could not create output dir" 1>&2
+		exit 1
+	fi
+	python backup.py --path "$b" --outpath "$ob" --tmppath "$tmppath" --csize 1000000000
+done
